a4ef0d93deec7846fb7aedc043f88f1d10def299
XeryusTC <armadillo@onenetbeyond.org>
2016-03-19 16:03:00 +0100

    Make a page to play the game

--- mas/guesswho/urls.py
+++ mas/guesswho/urls.py
@@ -3,4 +3,6 @@ from guesswho import views
 
 urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='home'),
+    url(r'^play/$', views.PlayView.as_view(), name='play'),
+    url(r'^play/reset/$', views.reset_game, name='reset'),
 ]
--- mas/guesswho/views.py
+++ mas/guesswho/views.py
@@ -1,4 +1,15 @@
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+
+class PlayView(TemplateView):
+    template_name = 'play.html'
+
+
+def reset_game(request):
+    request.session.flush()
+    return HttpResponseRedirect(reverse('guesswho:play'))
