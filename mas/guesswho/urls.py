7c61d7bfc694faec1d2dccefab04b8ab5c5d5a08
XeryusTC <armadillo@onenetbeyond.org>
2016-03-21 00:09:33 +0100

    Make questions work and show accumulated knowledge

--- mas/guesswho/urls.py
+++ mas/guesswho/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^play/$', views.PlayView.as_view(), name='play'),
     url(r'^play/reset/$', views.reset_game, name='reset'),
     url(r'^play/char/([a-zA-Z]+)/$', views.pick_char, name='char'),
+    url(r'^play/question/([a-zA-Z]+)/$', views.question, name='question'),
 ]
--- mas/guesswho/views.py
+++ mas/guesswho/views.py
@@ -10,8 +10,13 @@ characters = {
     'Jake': ['glasses', 'hat'],
     'John': ['hat', 'bald'],
     'Richard': ['hat', 'beard'],
-    'Sam': ['bald', 'beardt'],
+    'Sam': ['bald', 'beard'],
 }
+
+# States of knowledge
+NO     = 1
+YES    = 2
+UNKNOWN = 3
 
 class HomeView(TemplateView):
     template_name = 'home.html'
@@ -23,6 +28,27 @@ class PlayView(TemplateView):
     def get_context_data(self, *args, **kwargs):
         context = super(PlayView, self).get_context_data(*args, **kwargs)
         context['chars'] = characters
+        try:
+            context['npc_knows'] = []
+            for k, v in self.request.session['npc_knows'].items():
+                if v == YES:
+                    context['npc_knows'].append(k)
+                elif v == NO:
+                    context['npc_knows'].append('&not;' + k)
+                elif v == UNKNOWN:
+                    context['npc_knows'].append(k + ' &#8897; &not;' +  k)
+
+            context['player_knows'] = []
+            for k, v in self.request.session['player_knows'].items():
+                if v == YES:
+                    context['player_knows'].append(k)
+                elif v == NO:
+                    context['player_knows'].append('&not;' + k)
+                elif v == UNKNOWN:
+                    context['player_knows'].append(k + ' &#8897; &not;' + k)
+        except KeyError:
+            context['npc_knows'] = None
+            context['player_knows'] = None
         return context
 
 
@@ -34,6 +60,40 @@ def pick_char(request, name):
     if name in characters.keys():
         request.session['char'] = name
         request.session['npc_char'] = random.choice(list(characters.keys()))
+        request.session['npc_knows'] = {
+            'bald': UNKNOWN,
+            'beard': UNKNOWN,
+            'glasses': UNKNOWN,
+            'hat': UNKNOWN,
+        }
+        request.session['player_knows'] = {
+            'bald': UNKNOWN,
+            'beard': UNKNOWN,
+            'glasses': UNKNOWN,
+            'hat': UNKNOWN,
+        }
+        request.session['questions'] = []
     else:
         messages.warning(request, 'This is not a valid character')
+    return HttpResponseRedirect(reverse('guesswho:play'))
+
+def question(request, subject):
+    if subject == 'bald':
+        question = 'Is he bald?'
+    elif subject == 'beard':
+        question = 'Does he have a beard?'
+    elif subject == 'glasses':
+        question = 'Does he wear glasses?'
+    elif subject == 'hat':
+        question = 'Does he wear a hat?'
+
+    if subject in characters[request.session['npc_char']]:
+        question += ' Yes.'
+        request.session['player_knows'][subject] = YES
+    else:
+        question += ' No.'
+        request.session['player_knows'][subject] = NO
+    request.session['questions'].append(question)
+    request.session.modified = True
+
     return HttpResponseRedirect(reverse('guesswho:play'))
